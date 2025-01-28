ee0a70923aeeaa070f52861f000b2022a6b6fd91
B) <22089@portalcedi.edu.mx>
2025-01-28 10:30:22 -0600

    asdfgh

--- src/app/nasa.service.ts
+++ src/app/nasa.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +9,9 @@ export class NasaService {
   private apiUrl = 'https://api.nasa.gov/planetary/apod'
   private apiKey = 'Kv5Q65fO9MjkuObaSA97VtcKUtiLl66oJ1gVp9al'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getImageOTD(): Observable<any> {
+    return this.http.get(`${this.apiUrl}?api_key=${this.apiKey}`)
+  }
 }
