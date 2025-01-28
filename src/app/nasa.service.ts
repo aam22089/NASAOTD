850f5673d0dd7395c8432f087b79596768f18cea
B) <22089@portalcedi.edu.mx>
2025-01-28 08:55:47 -0600

    dfg

--- src/app/nasa.service.ts
+++ src/app/nasa.service.ts
@@ -4,6 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class NasaService {
+  private apiUrl = 'https://api.nasa.gov/planetary/apod'
+  private apiKey = 'Kv5Q65fO9MjkuObaSA97VtcKUtiLl66oJ1gVp9al'
 
   constructor() { }
 }
